56f44db45303713c621c11bc183ff20cf8f4f2b5
MihajloDTC2025 <mihajlo.drljaca@dtc.rs>
2026-08-27 16:10:05 +0200

    Finished main.py and data.json

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#test
+import  json
+
+# r - read
+with open("data.json",'r') as file:
+    data = json.load(file)
+    data.append({
+        "name":"Petar Petrovic",
+        "age":35,
+        "height":190,
+        "gender":"male"
+    })
+
+    print(data)
+
+    # w - write
+    with open("data.json",'w') as file:
+        json.dump(data,file, indent=4)
+
+
